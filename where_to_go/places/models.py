51bf56ca9c27f1503eec505380d8fa86e1ba9f0d
Aleksandr Semenov <semenlinux@gmail.com>
2021-05-18 11:04:56 +0300

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.base import Model
+from sort_order_field import SortOrderField
 
 
 class Place(models.Model):
@@ -10,3 +12,17 @@ class Place(models.Model):
 
     def __str__(self):
         return self.place_title
+
+
+class PlaceImage(models.Model):
+    sort_order = SortOrderField("Sort")
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name="place_images",
+        verbose_name="Место",
+    )
+    image_url = models.ImageField("Изображение места", upload_to="place_images")
+
+    def __str__(self) -> str:
+        return f"{ self.sort_order } {self.place.place_title}"
