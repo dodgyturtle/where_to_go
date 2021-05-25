a0fa88472134dd719f420ae3dbe3a34db292a90a
Aleksandr Semenov <semenlinux@gmail.com>
2021-05-25 21:21:47 +0300

    Use MTV and remove handlers

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,32 +1,9 @@
 import json
 import os
 
-from django.conf import settings
+
 from django.db import models
-from django.urls import reverse
 from tinymce.models import HTMLField
-
-
-class PlaceQuerySet(models.QuerySet):
-    def fetch_places_geojson(self):
-        geojson = {"type": "FeatureCollection", "features": []}
-        for place in self:
-            feature = {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [place.longitude, place.latitude],
-                },
-                "properties": {
-                    "title": place.place_title,
-                    "placeId": place.slug,
-                    "detailsUrl": reverse(
-                        "place_details", kwargs={"place_id": place.id}
-                    ),
-                },
-            }
-            geojson["features"].append(feature)
-        return geojson
 
 
 class Place(models.Model):
@@ -37,14 +14,13 @@ class Place(models.Model):
     latitude = models.FloatField("Широта", null=True, blank=True)
     longitude = models.FloatField("Долгота", null=True, blank=True)
 
-    objects = PlaceQuerySet.as_manager()
-
     def __str__(self):
         return self.place_title
-    
+
     class Meta:
-        verbose_name = 'Место отдыха'
-        verbose_name_plural = 'Места отдыха'
+        verbose_name = "Место отдыха"
+        verbose_name_plural = "Места отдыха"
+
 
 class PlaceImage(models.Model):
     sort_order = models.IntegerField("Sort", default=0, blank=False, null=False)
@@ -55,11 +31,11 @@ class PlaceImage(models.Model):
         verbose_name="Место",
     )
     image_url = models.ImageField("Изображение места", upload_to="place_images")
-    
+
     class Meta(object):
-        ordering = ['sort_order']
-        verbose_name = 'Изображение места отдыха'
-        verbose_name_plural = 'Изображение мест отдыха'
-    
+        ordering = ["sort_order"]
+        verbose_name = "Изображение места отдыха"
+        verbose_name_plural = "Изображение мест отдыха"
+
     def __str__(self) -> str:
         return f"{ self.sort_order } {self.place.place_title}"
--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,13 +1,45 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .handlers import generate_place_details
+
 from .models import Place
 
 
+def generate_place_details(place):
+    place_details = {
+        "title": place.place_title,
+        "imgs": [image.image_url.url for image in place.place_images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {"lng": place.longitude, "lat": place.latitude},
+    }
+    return place_details
+
+
+def fetch_places_geojson(places):
+    geojson = {"type": "FeatureCollection", "features": []}
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude],
+            },
+            "properties": {
+                "title": place.place_title,
+                "placeId": place.slug,
+                "detailsUrl": reverse("place_details", kwargs={"place_id": place.id}),
+            },
+        }
+        geojson["features"].append(feature)
+    return geojson
+
+
 def places(request):
-    places = Place.objects.all().fetch_places_geojson()
-    return render(request, "index.html", context={"places": places})
+    places = Place.objects.all()
+    places_geojson = fetch_places_geojson(places)
+    return render(request, "index.html", context={"places": places_geojson})
 
 
 def place_details_view(request, place_id):
